30918351a584737d3ea661cf30d9add7d872ecb2
xp1002 <xptan1002@gmail.com>
2018-03-18 16:53:39 -0700

    treemap

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+console.log('Hello from main.js');
+
+d3.selectAll("text").attr("fill", "white");
+
+d3.csv('dataWithGenres.csv',
+    function(datum){ 
+        // get all of our data into the requisite format first
+        return {
+            "Position" : +datum["Position"],
+            "Track" : datum["Track Name"],
+            "Artist" : datum["Artist"], 
+            "Streams" : +datum["Streams"],
+            "URL" : datum["URL"], 
+            "Date" : datum["Date"],
+            "Region" : datum["Region"],
+            "trackID" : datum["trackID"],
+            "genre" : datum['genre']
+        };
+    }, function(data) {
+
+        var newdata = data.filter(val => {return val.Region == "global"; });
+
+        var treemap = new Treemap(d3.select(".vis2"), newdata, 
+            function() {});
+
+        // var sunburstChart = new SunburstChart(d3.select(".vis1"), data, function() {});
+        // var parallelChart = new ParallelChart(d3.select(".vis3"), data, function() {
+        //     barChart.update(parallelChart.newData);
+        //     sunburstChart.update(parallelChart.newData,[]);
+        // });
+});
