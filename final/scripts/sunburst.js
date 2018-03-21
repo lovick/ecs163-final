5d2a22cc995817406dfcad5752d05f9bbbef04da
prim0rdia1 <kimdang826@gmail.com>
2018-03-20 17:48:31 -0700

    adding map

--- final/scripts/sunburst.js
+++ final/scripts/sunburst.js
@@ -1,7 +1,8 @@
 var sunTable = new Array, sunQuery = "table";  //default has all available countries, all available songs
 var mapSVG, artistQuery = "";
-var keys =  ["PHL", "USA", "DEU", "BRA", "SWE", "MEX", "ESP", "NLD", "GBR", "AUS"];
-var skeys = ["ph", "us", "de", "br", "se", "mx", "es", "nl", "gb", "au", "global"];
+var keys =  ["Philippines", "United States", "Germany", "Brazil", "Sweden", "Mexico",
+    "Spain", "Netherlands", "United Kingdom", "Australia", "Global", "Unselected"];
+var skeys = ["ph", "us", "de", "br", "se", "mx", "es", "nl", "gb", "au", "global", "table"];
 
 var mapdata = {
     PHL: { fillKey: 'Overdue' },
@@ -171,7 +172,7 @@ function updateSunburstChart(table){
         .append("svg:title")
         .text(function(d){
             var s;
-            if (d.depth <= 2){
+            if (d.depth <= 1){
                 if (d.depth == 1){
                     var keys =  ["Philippines", "USA", "Germany", "Brazil", "Sweden", "Mexico",
                         "Spain", "Netherlands", "Great Britain", "Australia", "Global", "Unselected"];
@@ -182,8 +183,7 @@ function updateSunburstChart(table){
                 }
             } else {
                 s = d.data.name
-                    + "\nby " + d.data.artist
-                    + "\n(" + formatNumber(d.data.streams) + ")";
+                    + "\n(" + formatNumber(d.data.streams) + " streams)";
             }
             return s;
         });
@@ -301,4 +301,89 @@ function changeSunQueryLabel(){
     var text2 = document.createTextNode(string);
     text.appendChild(text2);
     p.appendChild(text);
+    changeNation(sunQuery);
+}
+
+
+var arr = [];
+var countries;
+function drawSunMap(){
+    var width = 560,
+        height = 400;
+
+    var projection = d3.geoKavrayskiy7()
+        .scale(100)
+        .translate([width / 2, height / 2])
+        .precision(.1);
+
+    var path = d3.geoPath().projection(projection);
+
+    var svg = d3.select("#sunmap").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    queue()
+        .defer(d3.json, "./world-110m.json")
+        .defer(d3.tsv, "./world-country-names.tsv")
+        .await(ready);
+
+    function ready(error, world, names) {
+        if (error) throw error;
+
+        countries = topojson.feature(world, world.objects.countries).features;
+
+        //map country names to IDs used on map
+        arr=[];
+        names.forEach(function(i){
+            //console.log(i.name);
+            arr[i.id]=i.name;
+        });
+
+        svg.selectAll(".country")
+            .data(countries)
+            .enter().insert("path", ".graticule")
+            .attr("class", "suncountry")
+            .attr("d", path)
+            .style("fill", function(d, i) {
+                var n = arr[d.id];
+                var sq = keys[skeys.indexOf(sunQuery)];
+                if (keys.indexOf(n) >= 0){
+                    if (sq == n){
+                        return "#1e3699";
+                    }
+                    return "#86BBD8";
+                } else {
+                    return "#CCCCCC";
+                }
+            });
+
+        svg.insert("path", ".graticule")
+            .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+            .attr("class", "boundary")
+            .attr("d", path);
+    }
+
+    d3.select(self.frameElement).style("height", height + "px");
+}
+
+function changeNation(nat){
+    console.log("here from drawsunmap");
+
+    d3.select("#sunmap").selectAll(".suncountry")
+        .style("fill", function(d, i){
+            if (nat == "global"){
+                return "#1e3699";
+            }
+
+            var n = arr[d.id];
+            var sq = keys[skeys.indexOf(nat)];
+            if (keys.indexOf(n) >= 0){
+                if (sq == n){
+                    return "#1e3699";
+                }
+                return "#86BBD8";
+            } else {
+                return "#CCCCCC";
+            }
+        })
 }
